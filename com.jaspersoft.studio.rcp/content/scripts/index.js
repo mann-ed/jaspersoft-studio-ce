9be11a7cadd10dcff3b37f9b911fdfd8bf95f19d
Giulio Toffoli <gtoffoli@tibco.com>
2019-09-24 13:11:11 +0100

    Updated index.xhtml to use latest jquery and css2

--- com.jaspersoft.studio.rcp/content/scripts/index.js
+++ com.jaspersoft.studio.rcp/content/scripts/index.js
@@ -1,47 +1,19 @@
+/*
+ * Copyright 2018 - 2019  TIBCO Software Inc. All rights reserved.
+ * 
+ * -----
+ * File Created: Wednesday, 18th September 2019 10:32:37 am
+ * Last Modified: Tuesday, 24th September 2019 11:51:41 am
+ * Modified By: gtoffoli (gtoffoli@tibco.com>)
+ * -----
+ * Author: gtoffoli (gtoffoli@tibco.com)
+ */
 
 		$(document).ready(function(){
 				
-                var bgSize = Modernizr.backgroundsize;
-            
-                
-                $("#title").css({ opacity: 0});
-                $("#title").removeClass("hidden");
-
-                $(".widget").css({ opacity: 0});
-                $(".widget").removeClass('hidden');
-
-                $(".widget").css({ 'padding-top': ($(window).height()*2) + 'px'});
-                $(".widget").animate({ opacity: 1, 'padding-top': '0px' }, 2000,'easeOutQuart');	
-                $("#title").animate({ opacity: 1 }, 2000, function() { 
-
-                    $(".widget").addClass("postAnim");
-                    //$(".widget").css({ 'transition': '0.2s ease-in-out'});
-                });
-
-
-                $("#getstarted a").on('click', function(e) {
-                        e.preventDefault();
-
-                        var self = $(this);
-
-                        $("#getstarted a").animate({ 'margin-left': '-400px','opacity': 0 },500, function() {
-                            window.location=self.attr("href");
-                        });
-                        $("#learnmore a").animate({ 'margin-top': '400px','opacity': 0 },500);
-                        $("#collaborate a").animate({ 'margin-right': '-400px','opacity': 0 },500);
-
-                });
-            
-                if (!bgSize)
-                {
-                    $(".getstarted").css("background-image","none");
-                    $(".getstarted").append('<img src="images/pen.png" width="297" height="256" border="0">');
-                    
-                    $(".learn").css("background-image","none");
-                    $(".learn").append('<img src="images/learn.png" width="297" height="256" border="0">');
-                    
-                    $(".collaborate").css("background-image","none");
-                    $(".collaborate").append('<img src="images/collaborate.png" width="297" height="256" border="0">');
-                }
-                
-			});
+                setTimeout( function() {
+                    $("#title").addClass("enter");
+                    $(".widget").addClass("enter");
+                }, 250);
+        
+        });
