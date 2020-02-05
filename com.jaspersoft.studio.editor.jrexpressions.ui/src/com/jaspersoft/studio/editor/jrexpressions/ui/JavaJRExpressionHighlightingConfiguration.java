80e09cb600d479833b70f76a8b1f5da13ff6eb3b
Massimo Rabbi <mrabbi@tibco.com>
2020-02-05 16:57:23 +0100

    JIRA TIBCO #JSS-2721: fixed some glitches for expression editor when using dark theme.

--- com.jaspersoft.studio.editor.jrexpressions.ui/src/com/jaspersoft/studio/editor/jrexpressions/ui/JavaJRExpressionHighlightingConfiguration.java
+++ com.jaspersoft.studio.editor.jrexpressions.ui/src/com/jaspersoft/studio/editor/jrexpressions/ui/JavaJRExpressionHighlightingConfiguration.java
@@ -5,10 +5,12 @@
 package com.jaspersoft.studio.editor.jrexpressions.ui;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
+
+import com.jaspersoft.studio.utils.UIUtil;
 
 /**
  * Custom class containing the list of available configurations for the JRExpression(s) elements.
@@ -24,21 +26,26 @@ public class JavaJRExpressionHighlightingConfiguration extends DefaultHighlighti
 	public static final String FUNCTION_METHOD="Library function"; //$NON-NLS-1$
 	public static final String RESOURCE_BUNDLE_KEY="ResourceBundle Key"; //$NON-NLS-1$
 	
+	public static final String COLOR_DEFINITION_PREFIX = "com.jaspersoft.studio.editor.jrexpressions.ui.colors.";	//$NON-NLS-1$
+	public static final String PARAMETER_COLOR_DEFINITION_ID = COLOR_DEFINITION_PREFIX + "PARAMETER_TOKEN";	//$NON-NLS-1$
+	public static final String FIELD_COLOR_DEFINITION_ID = COLOR_DEFINITION_PREFIX + "FIELD_TOKEN";	//$NON-NLS-1$
+	public static final String VARIABLE_COLOR_DEFINITION_ID = COLOR_DEFINITION_PREFIX + "VARIABLE_TOKEN";	//$NON-NLS-1$
+	public static final String FUNCTIONMETHOD_COLOR_DEFINITION_ID = COLOR_DEFINITION_PREFIX + "FUNCTION_METHOD";	//$NON-NLS-1$
+	public static final String RBKEY_COLOR_DEFINITION_ID = COLOR_DEFINITION_PREFIX + "RESOURCE_BUNDLE_KEY";	//$NON-NLS-1$
+	
 	@Override
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		super.configure(acceptor);
-		addElementConfiguration(acceptor, PARAM_TOKEN, 187, 29, 29, SWT.BOLD);
-		addElementConfiguration(acceptor, VARIABLE_TOKEN, 0, 0, 255, SWT.BOLD);
-		addElementConfiguration(acceptor, FIELD_TOKEN, 0, 178, 0, SWT.BOLD);
-		addElementConfiguration(acceptor, FUNCTION_METHOD, 0, 0, 0, SWT.ITALIC);
-		addElementConfiguration(acceptor, RESOURCE_BUNDLE_KEY, 102, 46, 153, SWT.BOLD);
+		addElementConfiguration(acceptor, PARAM_TOKEN, UIUtil.getColor(PARAMETER_COLOR_DEFINITION_ID), SWT.BOLD);	//dark theme: same	
+		addElementConfiguration(acceptor, VARIABLE_TOKEN, UIUtil.getColor(VARIABLE_COLOR_DEFINITION_ID), SWT.BOLD);	//dark theme: 0 100 255
+		addElementConfiguration(acceptor, FIELD_TOKEN, UIUtil.getColor(FIELD_COLOR_DEFINITION_ID), SWT.BOLD);	//dark theme: same
+		addElementConfiguration(acceptor, FUNCTION_METHOD, UIUtil.getColor(FUNCTIONMETHOD_COLOR_DEFINITION_ID), SWT.ITALIC);//dark theme: same
+		addElementConfiguration(acceptor, RESOURCE_BUNDLE_KEY, UIUtil.getColor(RBKEY_COLOR_DEFINITION_ID), SWT.BOLD);//dark theme: 139 46 153
 	}
 
-	public void addElementConfiguration(IHighlightingConfigurationAcceptor acceptor, String s,
-			int r, int g, int b, int style) {
-		TextStyle textStyle = new TextStyle();
-		textStyle.setBackgroundColor(new RGB(255, 255, 255));
-		textStyle.setColor(new RGB(r, g, b));
+	public void addElementConfiguration(IHighlightingConfigurationAcceptor acceptor, String s, Color c, int style) {
+		TextStyle textStyle = new TextStyle(); 
+		textStyle.setColor(c.getRGB());
 		textStyle.setStyle(style);
 		acceptor.acceptDefaultHighlighting(s, s, textStyle);
 	}
