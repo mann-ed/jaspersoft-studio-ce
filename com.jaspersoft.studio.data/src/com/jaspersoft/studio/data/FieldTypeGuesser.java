bed4fc9e335422850c765d1c83e12fdbd1b3285d
Massimo Rabbi <mrabbi@tibco.com>
2021-02-26 12:14:46 +0100

    Fix for JIRA TIBCO #JSS-2946. Improved "read fields" operation for the Excel data sources.

--- com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/FieldTypeGuesser.java
+++ com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/FieldTypeGuesser.java
@@ -9,13 +9,20 @@ import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.poi.ss.usermodel.BuiltinFormats;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.data.AbstractPoiXlsDataSource;
 import net.sf.jasperreports.engine.design.JRDesignField;
 
 public class FieldTypeGuesser {
@@ -46,6 +53,88 @@ public class FieldTypeGuesser {
 				}
 			}
 			hasNext = ds.next();
+		}
+	}
+	
+	/** 
+	 * Dedicated type guessing method for the data sources related to Excel.
+	 * <p>
+	 * We try to base our detection on the Data Format information set on the cell
+	 * and rely on some cases also on the content.<br/>
+	 * We do not perform a full deep reading of all the rows, because it would be 
+	 * too expensive. We stop once we find a "valid" proposal for the type other than 
+	 * the usual fallback String one.
+	 * </p>
+	 * 
+	 * @param xlsDS the Excel type data source
+	 * @param columns the list of columns
+	 * @param monitor the progress monitor
+	 * @throws JRException
+	 * 
+	 * @see {@link BuiltinFormats}
+	 */
+	public static void guessTypes(AbstractPoiXlsDataSource xlsDS, List<JRDesignField> columns, IProgressMonitor monitor) throws JRException {
+		List<JRDesignField> cols = new CopyOnWriteArrayList<>(columns);
+		boolean hasNext = true;
+		for(int i=0;i<SAMPLESIZE && hasNext && !cols.isEmpty();i++) {
+			if (monitor != null && monitor.isCanceled()) {
+				return;
+			}
+			for(JRDesignField f : cols) {
+				try {
+					// Let's get the possible data format pattern
+					// see org.apache.poi.ss.usermodel.BuiltinFormats
+					// Fallback to 0, "General"
+					String dataFormat = xlsDS.getFieldFormatPattern(f);
+					int dataFormatID = (dataFormat!=null) ? BuiltinFormats.getBuiltinFormat(dataFormat) : 0;
+					BuiltinFormats.getBuiltinFormat(dataFormat);
+					// Let's get the string from excel and let's try to guess
+					String val = xlsDS.getStringFieldValue(f);
+					if(!StringUtils.isNotEmpty(val)) {
+						continue;
+					}
+					if(NumberUtils.isParsable(val)) {
+						if(dataFormatID==1) {
+							f.setValueClass(Long.class);	
+						}
+						else {
+							f.setValueClass(Double.class);
+						}
+						cols.remove(f);
+					}
+					else {
+						// try handle boolean case
+						Boolean boolVal = BooleanUtils.toBooleanObject(val);
+						if(boolVal!=null) {
+							f.setValueClass(Boolean.class);
+							cols.remove(f);
+						}
+						else {
+							// let's try some stuff for the dates
+							boolean isDate = false;
+							if(dataFormatID >= 0x0E && dataFormatID <= 0x16) {
+								isDate=true;
+							}
+							if(!isDate && xlsDS.getDateFormat()!=null) {
+								xlsDS.getDateFormat().parse(val);
+								isDate = true;
+							}
+							if(!isDate && dataFormat!=null) {
+								DateUtils.parseDate(val, dataFormat);
+								isDate = true;								
+							}
+							if(isDate) {
+								f.setValueClass(java.util.Date.class);
+								cols.remove(f);
+							}
+						}
+					}										
+				} catch (JRException | ParseException e) {
+					f.setValueClass(String.class);
+					cols.remove(f);
+				}
+			}
+			hasNext = xlsDS.next();
 		}
 	}
 
