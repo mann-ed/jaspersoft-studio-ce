90a9b01edf9674cf4f8a0f113a18ff48840db031
Massimo Rabbi <mrabbi@tibco.com>
2021-09-27 16:05:06 +0200

    Fixing the HTTP resources behavior for SWT browser widget. This workaround prevents the ATS (Apple Transport Security) warning message from appearing.

--- com.jaspersoft.studio.rcp.macosx/src/com/jaspersoft/studio/rcp/macosx/Startup.java
+++ com.jaspersoft.studio.rcp.macosx/src/com/jaspersoft/studio/rcp/macosx/Startup.java
@@ -4,6 +4,9 @@
  ******************************************************************************/
 package com.jaspersoft.studio.rcp.macosx;
 
+import org.eclipse.swt.internal.cocoa.NSBundle;
+import org.eclipse.swt.internal.cocoa.NSDictionary;
+import org.eclipse.swt.internal.cocoa.NSNumber;
 import org.eclipse.swt.internal.cocoa.NSString;
 import org.eclipse.swt.internal.cocoa.NSUserDefaults;
 import org.eclipse.swt.widgets.Display;
@@ -14,7 +17,8 @@ import net.sf.jasperreports.eclipse.ui.util.UIUtils;
 /**
  * Startup implementation to provide a fix/workaround for the problem exposed in:
  * - https://community.jaspersoft.com/jaspersoft-studio/issues/11071
- * - https://bugs.eclipse.org/bugs/show_bug.cgi?id=530315 
+ * - https://bugs.eclipse.org/bugs/show_bug.cgi?id=530315
+ * - https://bugs.eclipse.org/bugs/show_bug.cgi?id=568749 
  * 
  * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
  *
@@ -30,6 +34,14 @@ public class Startup implements IStartup {
 			defaults.setInteger(0, NSString.stringWith("NSAutomaticQuoteSubstitutionEnabled"));
 			defaults.setInteger(0, NSString.stringWith("NSAutomaticDashSubstitutionEnabled"));
 		}
+		
+		// Allows to use http resources in SWT browser to prevent the ATS (Apple Transport Security) error
+		// See the bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=568749
+		NSDictionary allowNonHttps = NSDictionary.dictionaryWithObject(
+				NSNumber.numberWithBool(true),
+				NSString.stringWith("NSAllowsArbitraryLoads"));
+		NSBundle.mainBundle().infoDictionary().setValue(
+				allowNonHttps, NSString.stringWith("NSAppTransportSecurity"));
 	}
 
 }
